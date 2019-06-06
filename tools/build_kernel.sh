d6992a801e9028c8bcdf0774e9142d0d37f7c1f0
Claus Marxmeier <claus@marxmeier.de>
2019-06-06 22:18:15 +0200

    Create build_kernel.sh

---
+++ tools/build_kernel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# Add the toolchain to $PATH
+echo "export PATH=/opt/hisi-linux-nptl/arm-hisiv100-linux/target/bin:\$PATH" >> ~/.bashrc
+source ~/.bashrc
+
+# Test the toolchain
+#cd ../../../mpp2/sample/ && make
+
+cd Hi3518_SDK_V1.0.7.0/osdrv
+#SHELL := /bin/bash
+make OSDRV_CROSS=arm-hisiv100nptl-linux CHIP=hi3518c all
+
