a78e8d553b820e008562826c2e9a7ab2f3c1644d
Claus Marxmeier <claus@marxmeier.de>
2019-07-19 11:31:42 +0200

    Create build_mpp2_90.sh

---
+++ tools/build_mpp2_90.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# Mostly "borrowed" from http://nemon.org/ipcam-ipr1631x/#SDK
+
+if [ ! -d Hi3518_SDK_V1.0.9.0 ]
+then
+   echo "SDK not found."
+   exit
+fi
+
+
+# Add the toolchain to $PATH
+echo "export PATH=/opt/hisi-linux-nptl/arm-hisiv100-linux/target/bin:\$PATH" >> ~/.bashrc
+source ~/.bashrc
+
+# Test the toolchain
+cd Hi3518_SDK_V1.0.9.0/mpp2/sample/ && make -j1 V=s
+cd ../tools && make -j1 V=s
+
