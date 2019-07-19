c9699176356807137fc70bbda7fc3bea5558e5d4
Claus Marxmeier <claus@marxmeier.de>
2019-07-20 01:31:42 +0200

    Update build_drv_B0.sh

--- tools/build_drv_B0.sh
+++ tools/build_drv_B0.sh
@@ -22,4 +22,4 @@ cd ../hisi-irda &&  make SNS=ov9712 -j1 V=s
 cd ../hiuser &&  make SNS=ov9712 -j1 V=s
 cd ../mmz &&  make SNS=ov9712 -j1 V=s
 cd ../rtc &&  make SNS=ov9712 -j1 V=s
-cd ../wtdg  make SNS=ov9712 -j1 V=s
+cd ../wtdg && make SNS=ov9712 -j1 V=s
