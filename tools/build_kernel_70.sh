52806beff1ba5e0efa07ef78e49f33b6368c2c15
Claus Marxmeier <claus@marxmeier.de>
2019-07-19 11:26:00 +0200

    Update and rename build_kernel.sh to build_kernel_70.sh

--- tools/build_kernel_70.sh
+++ tools/build_kernel_70.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 
+# Mostly "borrowed" from http://nemon.org/ipcam-ipr1631x/#SDK
+
+if [ ! -d Hi3518_SDK_V1.0.7.0 ]
+then
+   echo "SDK not found."
+   exit
+fi
+
+
 # Add the toolchain to $PATH
 echo "export PATH=/opt/hisi-linux-nptl/arm-hisiv100-linux/target/bin:\$PATH" >> ~/.bashrc
 source ~/.bashrc
@@ -8,7 +17,8 @@ source ~/.bashrc
 # Test the toolchain
 #cd ../../../mpp2/sample/ && make
 
+echo "to switch from default /bin/sh from dash to bash -> dpkg-reconfigure dash"
+
 cd Hi3518_SDK_V1.0.7.0/osdrv
 #SHELL := /bin/bash
 make OSDRV_CROSS=arm-hisiv100nptl-linux CHIP=hi3518c all
-
